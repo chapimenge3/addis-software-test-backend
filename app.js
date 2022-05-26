01671ded34705ade53fa45e34285dd70b436b571
Chapi Menge <chapimenge3@gmail.com>
2022-05-26 13:17:02 +0300

    Add Allow Cors Header

--- app.js
+++ app.js
@@ -3,21 +3,23 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var dotenv = require('dotenv');
+var cors = require('cors')
 // load environment variables from .env file
 dotenv.config();
 
 // Load Database Connection
 require('./db/connections');
 
-var indexRouter = require('./routes/index');
-
 var app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
+var indexRouter = require('./routes/index');
 app.use('/', indexRouter);
 
 module.exports = app;
